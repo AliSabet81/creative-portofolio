9bbab9d0a0e83e4012dff53a61642180300d16af
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-27 16:04:51 +0330

    Harry potter 3d model

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import bg from "../../public/background/home-background.png";
 import RenderModel from "@/components/RenderModel";
 import Wizard from "@/components/models/Wizard";
+import Harry from "@/components/models/Harry";
 
 export default function Home() {
   return (
@@ -14,7 +15,7 @@ export default function Home() {
       />
       <div className="w-full h-screen">
         <RenderModel>
-          <Wizard />
+          <Harry />
         </RenderModel>
       </div>
     </main>
