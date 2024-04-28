038ec106db99ed4a441b8ce93891215714539fc5
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-28 15:41:24 +0330

    layout home btn

--- src/app/(subPages)/layout.js
+++ src/app/(subPages)/layout.js
@@ -1,7 +1,9 @@
+import HomeBtn from "@/components/HomeBtn";
+
 export default function SubPagesLayout({ children }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center py-20 px-16">
-      <button></button>
+      <HomeBtn/>
       {children}
     </main>
   );
--- src/app/(subPages)/projects/page.js
+++ src/app/(subPages)/projects/page.js
@@ -14,7 +14,7 @@ export default function Projects() {
         className="-z-50 top-0 left-0 fixed w-full h-full object-cover object-center opacity-25 "
       />
       <ProjectList projects={projectsData} />
-      <div className="flex items-center justify-center fixed top-20 left-0 h-screen">
+      <div className="flex items-center justify-center fixed top-20 -left-24 h-screen">
         <RenderModel>
           <Staff />
         </RenderModel>
