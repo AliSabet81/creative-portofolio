c972b790eacb3feffb6628478c0bc86f8434db2f
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-28 15:30:26 +0330

    setting layout for subpages

---
+++ src/app/(subPages)/layout.js
@@ -0,0 +1,8 @@
+export default function SubPagesLayout({ children }) {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center py-20 px-16">
+      <button></button>
+      {children}
+    </main>
+  );
+}
--- src/app/(subPages)/projects/page.js
+++ src/app/(subPages)/projects/page.js
@@ -1,18 +1,17 @@
 import Image from "next/image";
-import bg from "../../../public/background/projects-background.png";
+import bg from "../../../../public/background/projects-background.png";
 import ProjectList from "@/components/projects";
-import { projectsData } from "../data";
+import { projectsData } from "../../data";
 import RenderModel from "@/components/RenderModel";
 import { Staff } from "@/components/models/Staff";
 
 export default function Projects() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between relative">
+    <>
       <Image
         src={bg}
         alt="background-image"
-        fill
-        className="-z-50 w-full h-full object-cover object-center opacity-25"
+        className="-z-50 top-0 left-0 fixed w-full h-full object-cover object-center opacity-25 "
       />
       <ProjectList projects={projectsData} />
       <div className="flex items-center justify-center fixed top-20 left-0 h-screen">
@@ -20,6 +19,6 @@ export default function Projects() {
           <Staff />
         </RenderModel>
       </div>
-    </main>
+    </>
   );
 }
