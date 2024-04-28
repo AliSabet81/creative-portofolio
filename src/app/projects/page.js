a58b678b28959edb429c12358a2c7d14049b6444
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-28 12:26:30 +0330

    projects page 3d model

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import bg from "../../../public/background/projects-background.png";
 import ProjectList from "@/components/projects";
 import { projectsData } from "../data";
+import RenderModel from "@/components/RenderModel";
+import { Staff } from "@/components/models/Staff";
 
 export default function Projects() {
   return (
@@ -13,6 +15,11 @@ export default function Projects() {
         className="-z-50 w-full h-full object-cover object-center opacity-25"
       />
       <ProjectList projects={projectsData} />
+      <div className="flex items-center justify-center fixed top-20 left-0 h-screen">
+        <RenderModel>
+          <Staff />
+        </RenderModel>
+      </div>
     </main>
   );
 }
