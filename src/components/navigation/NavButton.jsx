68960e5f9f41fccdc9ef1dd95c1b2e8e97e3f609
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-28 12:15:20 +0330

    project layout

--- src/components/navigation/NavButton.jsx
+++ src/components/navigation/NavButton.jsx
@@ -43,7 +43,7 @@ const NavButton = ({ x, y, label, link, icon, newTab }) => {
       <Link
         href={link}
         target={newTab ? "_blank" : "_self"}
-        className="text-foreground rounded-full flex items-center justify-center bg-background/20 border border-accent/30 border-solid backdrop-blur-[6px] shadow-glass-inset hover:shadow-glass-sm"
+        className="text-foreground rounded-full flex items-center justify-center custom-bg"
         aria-label={label}
         name={label}
       >
