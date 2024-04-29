435c166683e2fa46f7a50d70ed346951096da8ff
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-29 11:33:51 +0330

    starting contact page

--- src/components/HomeBtn.jsx
+++ src/components/HomeBtn.jsx
@@ -7,7 +7,7 @@ const HomeBtn = () => {
     <Link
       href={"/"}
       target={"_self"}
-      className="text-foreground rounded-full flex items-center justify-center custom-bg fixed top-4 left-4 w-fit self-start"
+      className="text-foreground rounded-full flex items-center justify-center custom-bg fixed top-4 left-4 w-fit self-start z-50"
       aria-label={"home"}
       name={"home"}
     >
