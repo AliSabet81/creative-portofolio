3e0aa9f56c85312bfdeefe2631dd179f6678270a
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-30 13:17:02 +0330

    projects items animation

--- src/components/projects/ProjectLayout.jsx
+++ src/components/projects/ProjectLayout.jsx
@@ -1,9 +1,21 @@
+'use client'
+import { motion } from "framer-motion";
 import Link from "next/link";
 import React from "react";
 
+const item = {
+  hidden: {
+    opacity: 0,
+    y: 100,
+  },
+  show: { opacity: 1, y: 0 },
+};
+
+const NavLink = motion(Link);
 const ProjectLayout = ({ name, description, date, demoLink }) => {
   return (
-    <Link
+    <NavLink
+      variants={item}
       href={demoLink}
       target="_blank"
       className="text-sm md:text-base flex items-center justify-between w-full relative rounded-lg overflow-hidden p-4 md:p-6 custom-bg"
@@ -16,7 +28,7 @@ const ProjectLayout = ({ name, description, date, demoLink }) => {
       <p className="text-muted sm:wtext-foreground">
         {new Date(date).toDateString()}
       </p>
-    </Link>
+    </NavLink>
   );
 };
 
