df8028882767b3f395468fb97dae58cc16475d14
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-30 09:28:10 +0330

    projects page responsive

--- src/app/(subPages)/projects/page.js
+++ src/app/(subPages)/projects/page.js
@@ -14,7 +14,7 @@ export default function Projects() {
         className="-z-50 top-0 left-0 fixed w-full h-full object-cover object-center opacity-25 "
       />
       <ProjectList projects={projectsData} />
-      <div className="flex items-center justify-center fixed top-20 -left-24 h-screen">
+      <div className="flex items-center justify-center fixed top-16 lg:top-20 -translate-x-1/2 lg:translate-x-0 -z-10 left-1/2 lg:-left-24 h-screen">
         <RenderModel>
           <Staff />
         </RenderModel>
--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -3,7 +3,7 @@ import ProjectLayout from "./ProjectLayout";
 
 const ProjectList = ({ projects }) => {
   return (
-    <div className="w-full max-w-4xl px-16 space-y-8 flex flex-col items-center">
+    <div className="w-full max-w-auto xs:max-w-4xl px-4 mx-auto lg:px-16 space-y-6 md:space-y-8 flex flex-col items-center">
       {projects.map((project, index) => {
         return <ProjectLayout key={index} {...project} />;
       })}
