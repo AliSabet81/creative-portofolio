6f0de13b830995919a1ee7444494e24b1193a680
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-30 15:55:24 +0330

    adding music

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
 import FireFliesBackground from "@/components/FireFliesBackground";
+import Sound from "@/components/Sound";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -21,6 +22,8 @@ export default function RootLayout({ children }) {
       >
         {children}
         <FireFliesBackground />
+        <Sound />
+        <div id="my-modal" />
       </body>
     </html>
   );
