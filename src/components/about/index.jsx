d4563b044b9d8a301ecd3b0ef8d0aa8c96832e8e
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-30 13:09:49 +0330

    about items animation

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,19 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
-import clsx from "clsx";
 import React from "react";
-
-const ItemLayout = ({ children, className }) => {
-  return (
-    <div
-      className={clsx(
-        "custom-bg p-6 sm:p-8 rounded-xl flex items-center justify-center space-y-8",
-        className
-      )}
-    >
-      {children}
-    </div>
-  );
-};
+import ItemLayout from "./ItemLayout";
 
 const AboutDetails = () => {
   return (
