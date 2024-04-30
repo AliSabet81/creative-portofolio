3ac63c6456fc0fe26f92007db64553646c25a900
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-30 09:40:45 +0330

    contact page responsive

--- src/app/(subPages)/contact/page.js
+++ src/app/(subPages)/contact/page.js
@@ -10,12 +10,12 @@ export default function Contact() {
         alt="background-image"
         className="-z-50 top-0 left-0 fixed w-full h-full object-cover object-center opacity-25 "
       />
-      <article className="relative w-full flex flex-col items-center justify-center space-y-8">
-        <div className="flex flex-col items-center justify-center space-y-6 w-3/4">
+      <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
+        <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
           <h1 className="text-accent font-semibold text-center text-4xl capitalize">
             summen the wizard
           </h1>
-          <p className="text-center font-light ">
+          <p className="text-center font-light text-sm xs:text-base">
             Step into the circle of enchantment and weave your words into the
             fabric of the cosmos. Whether you seek to conjure collaborations,
             unlock mysteries, or simply share tales of adventure, your messages
