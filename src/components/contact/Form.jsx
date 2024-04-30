9cd8e8320aedbbeae2b783360e0eef25635024ff
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-30 14:08:47 +0330

    contact item animation

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -3,6 +3,25 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import emailjs from "@emailjs/browser";
 import { Toaster, toast } from "sonner";
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.3,
+      delayChildren: 0.2,
+    },
+  },
+};
+const item = {
+  hidden: {
+    scale: 0,
+  },
+  show: { scale: 1 },
+};
+
 const Form = () => {
   const {
     register,
@@ -58,12 +77,16 @@ const Form = () => {
   return (
     <>
       <Toaster richColors={true} />
-      <form
+      <motion.form
+        variants={container}
+        initial="hidden"
+        animate="show"
         onSubmit={handleSubmit(onSubmit)}
         className="max-w-md w-full flex flex-col items-center justify-center space-y-4"
       >
-        <input
+        <motion.input
           type="text"
+          variants={item}
           placeholder="name"
           {...register("name", {
             required: "This flied is required!",
@@ -79,7 +102,8 @@ const Form = () => {
             {errors.name.message}
           </span>
         )}
-        <input
+        <motion.input
+          variants={item}
           type="email"
           placeholder="email"
           {...register("email", { required: "This flied is required!" })}
@@ -91,7 +115,8 @@ const Form = () => {
           </span>
         )}
 
-        <textarea
+        <motion.textarea
+          variants={item}
           placeholder="message"
           {...register("message", {
             required: "This flied is required!",
@@ -112,12 +137,13 @@ const Form = () => {
           </span>
         )}
 
-        <input
+        <motion.input
+          variants={item}
           value="Cast your message!"
           className="px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cursor-pointer capitalize"
           type="submit"
         />
-      </form>
+      </motion.form>
     </>
   );
 };
